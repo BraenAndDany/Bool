0d1f9327a1a30a43854c9e70c882be678a827564
Max <Xetyq@yandex.ru>
2023-11-24 16:00:59 +0700

    Добавьте файлы проекта.

---
+++ Bool/Programm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization.Formatters;
+using System.Text;
+using System.Threading.Tasks;
+using booling;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            booli booling =  new booli();
+
+            bool first = false;
+            bool second = false;
+            Console.Write(" " + "A" + "        ");
+            Console.Write(" " + "B" + "       |");
+            Console.Write("No" + "    ");
+            Console.Write("And" + "    ");
+            Console.Write("Or" + "     ");
+            Console.Write("Xor" + "    ");
+            Console.Write("Xnor" + "   ");
+            Console.Write("Impl" + "   ");
+            Console.Write("NoImpl" + "    ");
+            Console.WriteLine();
+            for (int i = 0; i < 4; i++) {
+             if (i == 1) {
+                    first = false;
+                    second = true;
+             }else if (i == 2)
+                {
+                    first = true;
+                    second = false;
+                }else if (i == 3)
+                {
+                    second = true;
+                }
+
+                Console.Write(" " + first + "   ");
+                Console.Write(" " + second + "   |");
+                booling.No(first, second);
+                booling.And(first, second);
+                booling.Or(first, second);
+                booling.Xor(first, second);
+                booling.Xnor(first, second);
+                booling.Impl(first, second);
+                booling.NoImpl(first, second);
+                Console.WriteLine();
+            }
+
+
+
+        }
+    }
+}
---
+++ Bool/bool.cs
@@ -0,0 +1,88 @@
+﻿namespace booling;
+public class booli 
+{
+    bool first;
+    bool second;
+
+    public void No(bool first, bool second)
+    {
+        if(first != second)
+        {
+            Console.Write(true + "   ");
+        }
+        else
+        {
+            Console.Write(false + "   ");
+        }
+    }
+    public void And(bool first, bool second)
+    {
+        bool prov = true;
+        if (first==prov&&second==prov)
+        {
+            Console.Write(true + "   ");
+        }
+        else
+        {
+            Console.Write(false + "   ");
+        }
+    }
+    public void Or(bool first, bool second)
+    {
+        bool prov = true;
+        if (first == prov || second == prov)
+        {
+            Console.Write(true + "   ");
+        }
+        else
+        {
+            Console.Write(false + "   ");
+        }
+    }
+    public void Xor(bool first, bool second)
+    {
+        bool prov = true;
+        if (first == prov && second == prov)
+        {
+            Console.Write(false + "   ");
+        }
+        else
+        {
+            Console.Write(true + "   ");
+        }
+    }
+    public void Xnor(bool first, bool second)
+    {
+        if (first == second)
+        {
+            Console.Write(true + "   ");
+        }
+        else
+        {
+                Console.Write(false + "   ");
+        }
+    }
+    public void Impl(bool first, bool second)
+    {
+        if (first != true||first==second)
+        {
+            Console.Write(true + "   ");
+        }
+        else
+        {
+            Console.Write(false + "   ");
+        }
+    }
+    public void NoImpl(bool first, bool second)
+    {
+        if (first == second || first != false)
+        {
+            Console.Write(true + "   ");
+        }
+        else
+        {
+            Console.Write(false + "   ");
+        }
+    }
+}
+
